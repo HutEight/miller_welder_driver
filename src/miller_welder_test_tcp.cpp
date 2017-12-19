b3a1263b90389d0067e0ab58e6cf3a318e553696
William <sxh759@case.edu>
2017-12-19 09:49:19 -0500

    modified the test a bit

--- src/miller_welder_test_tcp.cpp
+++ src/miller_welder_test_tcp.cpp
@@ -26,24 +26,30 @@ int main(int argc, char *argv[])
   // ros::param::param<std::string>("~host", host, "192.168.1.1"); // TODO
   // ros::param::param<std::string>("~frame_id", frame_id, "laser");
   host = "192.168.1.55";
+  ROS_WARN("Using host name 192.168.1.55 \n Modify if you change the ip settings.");
   boost::asio::io_service io_service;
 
   shared_ptr<TCPSocket> socket = shared_ptr<TCPSocket>(new TCPSocket(io_service));
+  ROS_WARN("Expecting TCP port 44818..");
   shared_ptr<UDPSocket> io_socket = shared_ptr<UDPSocket>(new UDPSocket(io_service, 2222));
+  ROS_WARN("Expecting UDP port 2222..");
   MillerWelder millerWelder(socket, io_socket);
 
   try
   {
-  	cout << "    RN_DEBUG -- host is: " << host << endl;
+
     millerWelder.open(host);
-    cout << "    RN_DEBUG -- finished opening host" << endl;
+
   }
-  catch (std::runtime_error ex)
+  // catch (std::runtime_error ex)
+  catch (std::exception ex)
   {
     ROS_FATAL_STREAM("Exception caught opening session: " << ex.what());
     return -1;
   }
 
+  ROS_INFO("TCP connection establshed through PORT 0xAF12..");
+  
   // FIXME
   try
   {
